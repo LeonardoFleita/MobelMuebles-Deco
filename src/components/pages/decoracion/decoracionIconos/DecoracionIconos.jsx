e66baee46e35cacd49c9054d1cc1e22de7fb3e6e
Leonardo Fleita <leo.fleita@gmail.com>
2024-01-19 17:34:49 -0300

    aumento de tamaño de letra e íconos

--- src/components/pages/decoracion/decoracionIconos/DecoracionIconos.jsx
+++ src/components/pages/decoracion/decoracionIconos/DecoracionIconos.jsx
@@ -6,16 +6,19 @@ const DecoracionIconos = () => {
       <img
         src="https://res.cloudinary.com/drez01kou/image/upload/v1705515460/mobel-deco/decoHeader/ipfdnf60ej5wlunpee5n.png"
         alt="Lámparas ícono"
+        id="decoracionIconos01"
       />
 
       <img
         src="https://res.cloudinary.com/drez01kou/image/upload/v1705515460/mobel-deco/decoHeader/aq8clbe3mylbzfyn2bbp.png"
         alt="Espejos ícono"
+        id="decoracionIconos02"
       />
 
       <img
         src="https://res.cloudinary.com/drez01kou/image/upload/v1705515460/mobel-deco/decoHeader/kyxvz0pe2fh9souvvera.png"
         alt="Objetos ícono"
+        id="decoracionIconos03"
       />
     </section>
   );
--- src/components/pages/mueblesALaMedida/mueblesALaMedidaIconos/MueblesALaMedidaIconos.jsx
+++ src/components/pages/mueblesALaMedida/mueblesALaMedidaIconos/MueblesALaMedidaIconos.jsx
@@ -3,25 +3,25 @@ import React from "react";
 const MueblesALaMedidaIconos = () => {
   return (
     <section className="mueblesAMedidaIconosContainer">
-      <a href="#mueblesCocina">
+      <a href="#mueblesCocina" id="mueblesAMedidaIconos01">
         <img
           src="https://res.cloudinary.com/drez01kou/image/upload/v1705353715/mobel-deco/mueblesAMedidaHeader/nxardzrcjtvxtmoe9izv.png"
           alt="Cocina ícono"
         />
       </a>
-      <a href="#mueblesLiving">
+      <a href="#mueblesLiving" id="mueblesAMedidaIconos02">
         <img
           src="https://res.cloudinary.com/drez01kou/image/upload/v1705353715/mobel-deco/mueblesAMedidaHeader/t3crif3acvtvkkn5j1nf.png"
           alt="Living ícono"
         />
       </a>
-      <a href="#mueblesBano">
+      <a href="#mueblesBano" id="mueblesAMedidaIconos03">
         <img
           src="https://res.cloudinary.com/drez01kou/image/upload/v1705353715/mobel-deco/mueblesAMedidaHeader/do0v7szfr93kvq4qgbny.png"
           alt="Baño ícono"
         />
       </a>
-      <a href="#mueblesOtros">
+      <a href="#mueblesOtros" id="mueblesAMedidaIconos04">
         <img
           src="https://res.cloudinary.com/drez01kou/image/upload/v1705353715/mobel-deco/mueblesAMedidaHeader/m6wqqqe5flkvjkdq50n8.png"
           alt="Otros ícono"
